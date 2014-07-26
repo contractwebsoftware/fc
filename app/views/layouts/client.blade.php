82c5619342ad59fa0697c269bd0abcc1978e6a76
ben <ben@contractwebsoftware.com>
2014-07-26 10:51:14 -0700

    viewport

--- app/views/layouts/client.blade.php
+++ app/views/layouts/client.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Forcremation</title>
 	<link rel="stylesheet" href="{{ asset('packages/Bootflat/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/client.css') }}">
