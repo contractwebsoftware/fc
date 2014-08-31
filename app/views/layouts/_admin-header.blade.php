f40803f9f873b81333dc4335d83cda5e6df3f154
ben <ben@contractwebsoftware.com>
2014-08-31 12:08:22 -0700

    styling

--- app/views/layouts/_admin-header.blade.php
+++ app/views/layouts/_admin-header.blade.php
@@ -1,12 +1,13 @@
-<div id="navigation">
+<div id="navigation" class="admin-nav">
     <img src="{{ asset('img/logo.jpg') }}" alt="logo" id="logo">
-    <ul>
-            <li><a href="{{ action('AdminController@getCustomers') }}">Customers</a></li>
-            <li><a href="{{ action('AdminController@getProviders')}}">Providers</a></li>
-            <!--<li><a href="{{ action('AdminController@getAllUser') }}">All Users</a></li>-->
-            <li><a href="{{ action('AdminController@getBillingPage') }}">Billings</a></li>
-            <li><a href="{{ action('AdminController@getSetting') }}">Settings</a></li>
-            <li><a href="{{ action('UserController@getLogout')}}">Logout</a></li>
+   {{Request::path()}}
+    <ul class="nav nav-pills">
+            <li class="{{(strpos(Request::path(),'admin/customers')!==false || strpos(Request::path(),'clients/steps')!==false?'active':'')}}"><a href="{{ action('AdminController@getCustomers') }}">Customers</a></li>
+            <li class="{{(strpos(Request::path(),'admin/providers')!==false || strpos(Request::path(),'admin/edit-provider')!==false?'active':'')}}"><a href="{{ action('AdminController@getProviders')}}">Providers</a></li>
+            <!--<li class="{{(Request::path('admin/*')?'active':'')}}"><a href="{{ action('AdminController@getAllUser') }}">All Users</a></li>-->
+            <li class="{{(strpos(Request::path(),'admin/billing-page')!==false?'active':'')}}"><a href="{{ action('AdminController@getBillingPage') }}">Billings</a></li>
+            <li class="{{(strpos(Request::path(),'admin/setting')!==false?'active':'')}}"><a href="{{ action('AdminController@getSetting') }}">Settings</a></li>
+            <li ><a href="{{ action('UserController@getLogout')}}">Logout</a></li>
     </ul>
     <div class="clear"></div>
    
