45ee817469c18b5066ed048b76caad2fe11399ab
ben <ben@contractwebsoftware.com>
2014-12-21 16:24:34 -0800

    no message

--- app/views/admin/provider-edit.blade.php
+++ app/views/admin/provider-edit.blade.php
@@ -216,7 +216,7 @@
                 </div>
                 <div class="col-xs-8">
                     <label for="provider_files" class="col-xs-9 pull-right text-right">My Homepage URL: &nbsp;
-                        <a class="pull-right" href="http://www.forcremation.com/?provider={{urlencode($provider->business_name)}}" target="_blank">http://www.forcremation.com/?provider={{urlencode($provider->business_name)}}</a>
+                        <a class="pull-right" href="http://www.forcremation.com/?provider={{$provider->id}}" target="_blank">http://www.forcremation.com/?provider={{$provider->id}}</a>
                     </label>
                 </div>
             </div>
