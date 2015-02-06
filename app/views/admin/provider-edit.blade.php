e7dec3eb7d8dff67283435fbe40dd91da76143c6
ben <ben@contractwebsoftware.com>
2015-02-05 19:43:05 -0800

    no message

--- app/views/admin/provider-edit.blade.php
+++ app/views/admin/provider-edit.blade.php
@@ -1114,9 +1114,9 @@
                 <h3>Provder Help</h3>
                 <div class="form-group">
                     <div class="col-xs-12">
-                        <a href="http://www.forcremation.com/images/Provider-Set-up.mp4" class="video_layer">Provider Setup</a>
-                        <a href="http://www.forcremation.com/images/logging-in-forcremation.mp4" class="video_layer">Provider Login</a>
-                        <a href="http://www.forcremation.com/images/Provider-Pricing.mp4" class="video_layer">Provider Pricing</a>
+                        <a href="http://www.forcremation.com/images/Provider-Set-up.mp4" class="video_layer">Provider Setup</a><br />
+                        <a href="http://www.forcremation.com/images/logging-in-forcremation.mp4" class="video_layer">Provider Login</a><br />
+                        <a href="http://www.forcremation.com/images/Provider-Pricing.mp4" class="video_layer">Provider Pricing</a><br />
                         <a href="http://www.forcremation.com/images/Forms-ForCremation.mp4" class="video_layer">How To Upload Forms</a>
 
                         <br /><br />
