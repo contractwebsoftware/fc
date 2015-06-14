79ee9641289779566fa78904ae7dd2cfbabac38b
ben <ben@contractwebsoftware.com>
2015-06-14 10:55:10 -0700

    fix send sig

--- app/controllers/RightSignature.php
+++ app/controllers/RightSignature.php
@@ -178,8 +178,8 @@ class RightSignature
         try {
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_HEADER, 0);
-            curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+            #curl_setopt($curl, CURLOPT_HEADER, 0);
+            #curl_setopt($curl, CURLINFO_HEADER_OUT, true);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
