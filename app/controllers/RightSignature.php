4d3bc47e2d8324ac9851716c2eb0c1aa148ff837
ben <ben@contractwebsoftware.com>
2015-06-14 11:12:13 -0700

    no message

--- app/controllers/RightSignature.php
+++ app/controllers/RightSignature.php
@@ -178,8 +178,8 @@ class RightSignature
         try {
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
-            #curl_setopt($curl, CURLOPT_HEADER, 0);
-            #curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+            curl_setopt($curl, CURLOPT_HEADER, 0);
+            curl_setopt($curl, CURLINFO_HEADER_OUT, true);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
@@ -193,7 +193,6 @@ class RightSignature
                 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
 
                 curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml;charset=utf-8", "api-token: $this->secure_token"));
-
 
             }
 
