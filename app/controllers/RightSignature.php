3825395eb02f8393a388015bedd4ca5652c3d180
ben <ben@contractwebsoftware.com>
2015-08-02 11:00:03 -0700

    no message

--- app/controllers/RightSignature.php
+++ app/controllers/RightSignature.php
@@ -178,7 +178,6 @@ class RightSignature
         try {
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_HEADER,false);
             curl_setopt($curl, CURLINFO_HEADER_OUT, true);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -187,14 +186,33 @@ class RightSignature
             curl_setopt($curl, CURLOPT_HTTPHEADER, array("api-token: $this->secure_token"));
 
             if ($body) {
-                curl_setopt($curl, CURLOPT_POST, 1);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                #curl_setopt($curl, CURLOPT_POST, 1);
+                #curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+                #curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                
 
-                #curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml;charset=utf-8","api-token: ".$this->secure_token));
+                $headers = array(
+                    "Content-type: text/xml",
+                    "api-token: ".$this->secure_token,
+                    "Content-length: " . strlen($body),
+                    "Connection: close",
+                );
+
+                curl_setopt($ch, CURLOPT_URL,$url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+                #curl_setopt($curl, CURLOPT_CUSTOMREQUEST, array("Content-Type:text/xml;charset=utf-8","api-token: ".$this->secure_token));
 
             }
-            #else curl_setopt($curl, CURLOPT_HTTPHEADER, array("api-token: $this->secure_token")); // Set the headers.
+            else {
+                curl_setopt($curl, CURLOPT_HEADER,false);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array("api-token: $this->secure_token"));
+            } // Set the headers.
 
 
             if (FALSE === $curl)
