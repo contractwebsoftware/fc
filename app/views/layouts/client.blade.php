89c79c043cb4c78f8bf896915e7c99207cec0fa0
Ben Davol <ben@contractwebsoftware.com>
2017-07-30 11:18:25 -0700

    Revert "test"
    This reverts commit 7601b8ee437de1caccfbf9ccaa3a587392beb828.

--- app/views/layouts/client.blade.php
+++ app/views/layouts/client.blade.php
@@ -25,7 +25,7 @@
                 <link rel="stylesheet" href="<?=asset('css/client-no-frame.css')?>">
                 <script>
                     //$(".no-frame-container,.no-frame-container div:first").width("480px");
-                    $("body, .no-frame-container").width(window.parent.document.body.clientWidth);
+                    $("body, .no-frame-container").width(parent.document.body.clientHeight);
                 </script>
                 <?php
             }
