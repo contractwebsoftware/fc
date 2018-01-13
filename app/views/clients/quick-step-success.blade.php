9aa682494f392076598983acf22f0dcdc32b7ed5
ben <ben@contractwebsoftware.com>
2018-01-13 09:41:56 -0800

    quick form success

--- app/views/clients/quick-step-success.blade.php
+++ app/views/clients/quick-step-success.blade.php
@@ -3,13 +3,19 @@
 <?php
     if(!is_null(Session::get('no-frame')))$noframe = Session::get('no-frame');
     else $noframe = false;
+
+    if(is_object(Session::get('provider'))){
+        $provider = Session::get('provider');
+        $provider_name = 'from '.$provider->name;
+    }
+else $provider_name = '';
 ?>
 <div class="col-sm-12">
 
     <div name="success" id="success">
 
         <div class="alert alert-success">
-            <h3>Success, A Representative from Rogue Valley Cremation will call to confirm with the next regular business day</h3>
+            <h3>Success, A Representative {{ $provider->name }} will call to confirm with the next regular business day</h3>
         </div>
     </div>
 </div>
